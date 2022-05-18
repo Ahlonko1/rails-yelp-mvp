a35c21718cd30fab49cd66471cf96e048b050089
Ahlonko Kowouvi <ahlonko.kowouvi@gmail.com>
2022-05-18 22:32:28 +0200

    mvp

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -17,17 +17,21 @@ class RestaurantsController < ApplicationController
   def create
     @restaurant = Restaurant.new(restaurant_params)
     @restaurant.save
-    redirect_to restaurant_path(@restaurant)
+    if @restaurant.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   private
 
-  def restaurant_params
-    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
-  end
-
   def set_restaurant
     @restaurant = Restaurant.find(params[:id])
+  end
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
   end
 
 end
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,4 +1,7 @@
 class ReviewsController < ApplicationController
+
+  before_action :set_restaurant, only: %i[new create]
+
   def index
     @review = Review.all
   end
@@ -13,11 +16,20 @@ class ReviewsController < ApplicationController
 
   def create
     @review = Review.new(review_params)
+    @review.restaurant = @restaurant
     @review.save
-    redirect_to review_path(@review)
+    if @review.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   private
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:restaurant_id])
+  end
 
   def review_params
     params.require(:review).permit(:rating, :content, :created_at, :updated_at, :restaurant_id)
